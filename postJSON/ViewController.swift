bd7f350b0cc981d17a8164a5f46d35d18a3c752c
amishbanjara <52459243+amishbanjara@users.noreply.github.com>
2020-02-09 13:24:34 -0600

    Initial Commit

---
+++ postJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  postJSON
+//
+//  Created by Amish Banjara on 2/9/20.
+//  Copyright © 2020 Amish Banjara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
